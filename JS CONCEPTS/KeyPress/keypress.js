e6f6fac332260db1f7fce14b80a2188960348666
abhishek-dev <enquireabhishek@gmail.com>
2022-11-11 20:34:54 +0530

    keypress event in js

--- JS CONCEPTS/KeyPress/keypress.js
+++ JS CONCEPTS/KeyPress/keypress.js
@@ -1,3 +1,49 @@
-document.addEventListener("keydown", ()=>{
-    alert("keypressed");
+
+
+
+//detect key press
+ 
+document.addEventListener("keydown", (e)=>{
+    // alert("keypressed");
+    console.log(e.key)
 })
+
+
+
+//check which key is pressed
+function makesound(key){
+    switch(key) {
+        case "w":
+            alert("W pressed");
+            break;
+    
+        case "s":
+            alert("W pressed");
+            break;
+    
+        case "d":
+            alert("W pressed");
+            break;
+    
+        case "f":
+            alert("W pressed");
+            break;
+    
+        case "g":
+            alert("W pressed");
+            break;
+    
+        case "q":
+            alert("W pressed");
+            break;
+            case "t":
+                alert("T pressed");
+                break;
+
+             default:
+                    alert("invalid key pressed");
+                    break;
+    }
+    
+}
+
